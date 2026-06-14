d43ee42ecff87ec4e4867ac0786a1b842e45f917
Prakhar <playingprakhar122@gmail.com>
2026-06-14 12:08:19 +0530

    create jwt verify middleware

---
+++ Backend/middleware/jwtVerify.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const jwtVerify = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.userId = decoded.userId;
+
+  next();
+
+};
