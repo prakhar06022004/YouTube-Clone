25070626765fbe84d253ee7468ee0b840421a57c
Prakhar <playingprakhar122@gmail.com>
2026-06-29 23:58:26 +0530

    add few things

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -7,73 +7,76 @@ import { RiHistoryFill } from "react-icons/ri";
 import { MdOutlinePlaylistPlay } from "react-icons/md";
 import { MdSaveAlt } from "react-icons/md";
 import { SlLike } from "react-icons/sl";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Sidebar = ({ showSidebar, setShowSidebar }) => {
   const [active, setActive] = useState("Home");
   return (
     <div
       className={`fixed top-12 left-0 h-screen w-60 bg-black
-  transition-all duration-300 rounded flex flex-col gap-5 border-r border-b border-gray-600  ${showSidebar ? "translate-x-0" : "-translate-x-full"}`}
+  transition-all duration-300 rounded flex flex-col gap-5 border-r border-b border-gray-600 select-none ${showSidebar ? "translate-x-0" : "-translate-x-full"}`}
     >
-      
       <div className="py-5 flex flex-col gap-5">
         <div
-          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 ${active === "Home" ? "bg-gray-700" : ""}`}
+          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 cursor-pointer ${active === "Home" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Home")}
         >
           <FaHome size={25} color="white" />
-          <p className="text-white text-lg">Home</p>
+          <p className="text-white text-md">Home</p>
         </div>
 
         <div
-          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 ${active === "Shorts" ? "bg-gray-700" : ""}`}
+          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 cursor-pointer ${active === "Shorts" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Shorts")}
         >
           <SiYoutubeshorts size={25} color="white" />
-          <p className="text-white text-lg">Shorts</p>
+          <p className="text-white text-md">Shorts</p>
         </div>
 
         <div
-          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 ${active === "Subscription" ? "bg-gray-700" : ""}`}
+          className={`flex gap-7 items-center py-1 px-2 w-full rounded hover:bg-gray-800 duration-150 pl-5 cursor-pointer ${active === "Subscription" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Subscription")}
         >
           <MdOutlineSubscriptions size={25} color="white" />
-          <p className="text-white text-lg">Subscription</p>
+          <p className="text-white text-md">Subscription</p>
         </div>
       </div>
 
       <div className="flex flex-col gap-4 py-4">
-        <p className="text-white pl-5">You</p>
+        <div className="flex items-center gap-1">
+          <p className="text-white pl-5">You</p>
+          <IoIosArrowForward color="white" size={17} />
+        </div>
 
         <div
-          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 ${active === "History" ? "bg-gray-700" : ""}`}
+          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 cursor-pointer ${active === "History" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("History")}
         >
           <RiHistoryFill size={25} />{" "}
-          <p className="text-lg px-2 py-1">History</p>
+          <p className="text-md px-2 py-1">History</p>
         </div>
 
         <div
-          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 ${active === "Playlists" ? "bg-gray-700" : ""}`}
+          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 cursor-pointer ${active === "Playlists" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Playlists")}
         >
           <MdOutlinePlaylistPlay size={25} />
-          <p className="text-lg px-2 py-1">Playlists</p>
+          <p className="text-md px-2 py-1">Playlists</p>
         </div>
 
         <div
-          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 ${active === "Save videos" ? "bg-gray-700" : ""}`}
+          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 cursor-pointer ${active === "Save videos" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Save videos")}
         >
           <MdSaveAlt size={25} />{" "}
-          <p className="text-lg px-2 py-1">Save videos</p>
+          <p className="text-md px-2 py-1">Save videos</p>
         </div>
 
         <div
-          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 ${active === "Liked videos" ? "bg-gray-700" : ""}`}
+          className={`text-white flex items-center gap-7 pl-5 rounded hover:bg-gray-800 duration-150 cursor-pointer ${active === "Liked videos" ? "bg-gray-700" : ""}`}
           onClick={() => setActive("Liked videos")}
         >
-          <SlLike size={25} /> <p className="text-lg px-2 py-1">Liked videos</p>
+          <SlLike size={25} /> <p className="text-md px-2 py-1">Liked videos</p>
         </div>
       </div>
     </div>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../Components/Sidebar";
+import { IoMenu } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [showSidebar, setShowSidebar] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen bg-black p-4">
+      <div className="flex gap-6">
+        <IoMenu
+          color="white"
+          size={28}
+          onClick={() => setShowSidebar((prev) => !prev)}
+          className="cursor-pointer"
+        />
+        <div
+          className="flex items-center gap-1 cursor-pointer select-none"
+          onClick={() => navigate("/")}
+        >
+          <img src={"/youtube.png"} alt="YTlogo" className="w-8 h-8" />
+
+          <h1 className="text-2xl font-medium text-white">Youtube</h1>
+        </div>
+      </div>
+      <div>
+        <Sidebar setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
